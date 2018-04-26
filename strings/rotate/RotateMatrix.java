dacba95ddfc473516de23a5ee433d93d4001e3a5
Marius Kleiner <kleiner@gmail.com>
2018-04-26 18:38:59 +0200

    Implemented matrix rotations (Problem 1.7)

--- strings/rotate/RotateMatrix.java
+++ strings/rotate/RotateMatrix.java
@@ -7,7 +7,7 @@ public class RotateMatrix {
         int n = matrix.length;
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
-                matrix[i][j] = i*5 + j;
+                matrix[i][j] = i*n + j;
     }
 
     private static void printMatrix(int[][] matrix) {
@@ -19,20 +19,38 @@ public class RotateMatrix {
     }
 
     public static void rotateMatrix(int[][] img) {
+
+        int n = img.length;
+        for (int i = 0; i < n - 1 - i; i++)
+            rotateLevel(img, i);
+    }
+
+    private static void rotateLevel(int[][] img, int level) {
+
+        int n = img.length;
+        for (int i = level; i < n - level - 1; i++) {
+
+            int topLeft = img[level][i];
+            img[level][i] = img[n-1-i][level];
+            img[n-1-i][level] = img[n-1-level][n-1-i];
+            img[n-1-level][n-1-i] = img[i][n-1-level];
+            img[i][n-1-level] = topLeft;
+        }
     }
 
 
 
     public static void main(String[] args) {
 
-        int img[][] = new int[5][5];
+        int img[][] = new int[10][10];
         fillMatrix(img);
         System.out.println("Original matrix:");
         printMatrix(img);
 
-        rotateMatrix(img);
-
-        System.out.println("Rotated matrix:");
-        printMatrix(img);
+        for (int i = 1; i <= 4; i++) {
+            rotateMatrix(img);
+            System.out.println("Rotated matrix after " + i + " rotations:");
+            printMatrix(img);
+        }
     }
 }
